f36d54952b582ecfa9cde762795f6a00d196b29b
Pedro <peuhbs@gmail.com>
2022-02-16 21:13:16 -0300

    Show content, Drawer, Badge, Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 //Components
+import Item from "./Item/item";
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 //styles
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
 //Types
 export type CartItemType = {
   id: number;
@@ -23,15 +24,16 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products',
     getProducts
   );
-  console.log(data);
 
-    const getTotalItems = () => null;
+    const getTotalItems = (items: CartItemType[]) => null;
 
-    const handleAddToCart = () => null;
+    const handleAddToCart = (clickedItem: CartItemType) => null;
 
     const handleRemoveFromCart = () => null;
 
@@ -39,10 +41,24 @@ const App = () => {
     if (error) return <div>Something went wrong</div>
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  )
-}
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon/>
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
+};
 
 export default App
